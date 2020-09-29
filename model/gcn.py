aec19f3171f7b2047794b3520be2ccaba6342d0e
Zheng Tang <oscartang1120@gmail.com>
2020-09-29 04:50:53 -0700

    Update gcn.py

--- model/gcn.py
+++ model/gcn.py
@@ -148,8 +148,14 @@ class GCNRelationModel(nn.Module):
 
         print (adj.size())
         print (weights.size())
+        torch.set_printoptions(profile="full")
+        print (adj[0])
+        print (weights[0])
         adj = adj * weights
+        print (adj[0])
         adj = adj + adj.transpose(1, 2)
+
+        exit()
 
         # zero out adj for ablation
         if self.opt.get('no_adj', False):
@@ -174,7 +180,52 @@ class GCNRelationModel(nn.Module):
         outputs = self.out_mlp(outputs)
         return outputs, h_out
 
+class Attention(nn.Module):
+    """
+    A GCN layer with attention on deprel as edge weights.
+    """
+    
+    def __init__(self, input_size, query_size):
+        super(Attention, self).__init__()
+        self.input_size = input_size
+        self.query_size = query_size
+        # self.ulinear = nn.Linear(input_size, attn_size)
+        # self.vlinear = nn.Linear(query_size, attn_size, bias=False)
+        # self.tlinear = nn.Linear(attn_size, 1)
+        self.weight = nn.Parameter(torch.Tensor(input_size, query_size))
+        self.init_weights()
 
+    def init_weights(self):
+        # self.ulinear.weight.data.normal_(std=0.001)
+        # self.vlinear.weight.data.normal_(std=0.001)
+        # self.tlinear.weight.data.zero_() # use zero to give uniform attention at the beginning
+        self.weight.data.normal_(std=0.001)
+
+    def forward(self, x, x_mask, q):
+        """
+        x : batch_size * seq_len * input_size
+        q : batch_size * query_size
+        f : batch_size * seq_len * feature_size
+        """
+        batch_size, seq_len, _ = x.size()
+
+        # x_proj = self.ulinear(x.contiguous().view(-1, self.input_size)).view(
+        #     batch_size, seq_len, self.attn_size)
+        # q_proj = self.vlinear(q.view(-1, self.query_size)).contiguous().view(
+        #     batch_size, self.attn_size).unsqueeze(1).expand(
+        #         batch_size, seq_len, self.attn_size)
+        # projs = [x_proj, q_proj]
+        # scores = self.tlinear(torch.tanh(sum(projs)).view(-1, self.attn_size)).view(
+        #     batch_size, seq_len)
+
+        x_proj = torch.matmul(x, self.weight)
+        scores = torch.bmm(x_proj, q.view(batch_size, self.query_size, 1)).view(batch_size, seq_len)
+
+        # mask padding
+        scores.data.masked_fill_(x_mask.data, -float('inf'))
+        weights = F.softmax(scores, dim=1)
+
+        return weights
 
 def pool(h, mask, type='max'):
     if type == 'max':
