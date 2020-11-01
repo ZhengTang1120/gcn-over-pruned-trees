e36d1bf76fcd26d0d487eb2c735e63a7d28e3463
Zheng Tang <oscartang1120@gmail.com>
2020-11-01 01:20:22 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -74,8 +74,7 @@ class GCNTrainer(Trainer):
         self.classifier = GCNClassifier(opt, emb_matrix=emb_matrix)
         self.decoder = Decoder(opt)
         self.criterion = nn.CrossEntropyLoss()
-        self.parameters = [p for p in self.classifier.parameters() if p.requires_grad] 
-                        + [p for p in self.decoder.parameters() if p.requires_grad]
+        self.parameters = [p for p in self.classifier.parameters() if p.requires_grad] + [p for p in self.decoder.parameters() if p.requires_grad]
         if opt['cuda']:
             self.classifier.cuda()
             self.decoder.cuda()
