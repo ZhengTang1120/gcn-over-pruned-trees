402e2d17e6fc74fac108a5a041f3ccaeaefd0c4f
Zheng Tang <oscartang1120@gmail.com>
2020-11-04 04:53:41 -0700

    ablation test

--- data/loader.py
+++ data/loader.py
@@ -13,48 +13,36 @@ class DataLoader(object):
     """
     Load data from json files, preprocess and prepare batches.
     """
-    def __init__(self, filename, batch_size, opt, vocab, mappings, evaluation=False):
+    def __init__(self, filename, batch_size, opt, vocab, evaluation=False):
         self.batch_size = batch_size
         self.opt = opt
         self.vocab = vocab
         self.eval = evaluation
         self.label2id = constant.LABEL_TO_ID
-        self.mappings = mappings
 
         with open(filename) as infile:
             data = json.load(infile)
         self.raw_data = data
-        data, data_r = self.preprocess(data, vocab, opt)
+        data = self.preprocess(data, vocab, opt)
 
         # shuffle for training
         if not evaluation:
             indices = list(range(len(data)))
             random.shuffle(indices)
             data = [data[i] for i in indices]
-
-            indices = list(range(len(data_r)))
-            random.shuffle(indices)
-            data_r = [data_r[i] for i in indices]
-
         self.id2label = dict([(v,k) for k,v in self.label2id.items()])
-        self.labels = [self.id2label[d[-1]] for d in data] + [self.id2label[d[-2]] for d in data_r]
-        self.num_examples = len(data) + len(data_r)
+        self.labels = [self.id2label[d[-1]] for d in data]
+        self.num_examples = len(data)
 
         # chunk into batches
         data = [data[i:i+batch_size] for i in range(0, len(data), batch_size)]
-        data_r = [data_r[i:i+batch_size] for i in range(0, len(data_r), batch_size)]
-        self.data = data + data_r
-        print("{} batches created for {}".format(len(self.data), filename))
+        self.data = data
+        print("{} batches created for {}".format(len(data), filename))
 
     def preprocess(self, data, vocab, opt):
         """ Preprocess the data and convert to ids. """
         processed = []
-        processed_rule = []
-        with open(self.mappings) as f:
-            mappings = f.readlines()
-        with open('dataset/tacred/rules.json') as f:
-            rules = json.load(f)
-        for c, d in enumerate(data):
+        for d in data:
             tokens = list(d['token'])
             if opt['lower']:
                 tokens = [t.lower() for t in tokens]
@@ -75,13 +63,8 @@ class DataLoader(object):
             subj_type = [constant.SUBJ_NER_TO_ID[d['subj_type']]]
             obj_type = [constant.OBJ_NER_TO_ID[d['obj_type']]]
             relation = self.label2id[d['relation']]
-            if 't_' in mappings[c] or 's_' in mappings[c]:
-                rule = helper.word_tokenize(rules[eval(mappings[c])[0][1]])
-                rule = map_to_ids(rule, vocab.rule2id) 
-                rule = [constant.SOS_ID] + rule + [constant.EOS_ID]
-                processed_rule += [(tokens, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, relation, rule)]
             processed += [(tokens, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, relation)]
-        return processed, processed_rule
+        return processed
 
     def gold(self):
         """ Return gold labels as a list. """
@@ -99,7 +82,7 @@ class DataLoader(object):
         batch = self.data[key]
         batch_size = len(batch)
         batch = list(zip(*batch))
-        
+        assert len(batch) == 10
 
         # sort all fields by lens for easy RNN operations
         lens = [len(x) for x in batch[0]]
@@ -125,11 +108,7 @@ class DataLoader(object):
 
         rels = torch.LongTensor(batch[9])
 
-        if len(batch) == 11:
-            rule = get_long_tensor(batch[10], batch_size)
-            return (words, masks, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, rels, orig_idx, rule)
-        else:
-            return (words, masks, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, rels, orig_idx)
+        return (words, masks, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, rels, orig_idx)
 
     def __iter__(self):
         for i in range(self.__len__()):
@@ -137,6 +116,33 @@ class DataLoader(object):
 
 def map_to_ids(tokens, vocab):
     ids = [vocab[t] if t in vocab else constant.UNK_ID for t in tokens]
+    return ids
+
+def get_positions(start_idx, end_idx, length):
+    """ Get subj/obj position sequence. """
+    return list(range(-start_idx, 0)) + [0]*(end_idx - start_idx + 1) + \
+            list(range(1, length-end_idx))
+
+def get_long_tensor(tokens_list, batch_size):
+    """ Convert list of list of tokens to a padded LongTensor. """
+    token_len = max(len(x) for x in tokens_list)
+    tokens = torch.LongTensor(batch_size, token_len).fill_(constant.PAD_ID)
+    for i, s in enumerate(tokens_list):
+        tokens[i, :len(s)] = torch.LongTensor(s)
+    return tokens
+
+def sort_all(batch, lens):
+    """ Sort all fields by descending order of lens, and return the original indices. """
+    unsorted_all = [lens] + [range(len(lens))] + list(batch)
+    sorted_all = [list(t) for t in zip(*sorted(zip(*unsorted_all), reverse=True))]
+    return sorted_all[2:], sorted_all[1]
+
+def word_dropout(tokens, dropout):
+    """ Randomly dropout tokens (IDs) and replace them with <UNK> tokens. """
+    return [constant.UNK_ID if x != constant.UNK_ID and np.random.random() < dropout \
+            else x for x in tokens]
+
+cab[t] if t in vocab else constant.UNK_ID for t in tokens]
     return ids
 
 def get_positions(start_idx, end_idx, length):
