724e3632e5cbde90d3adcec7c69f65fe5efbec50
Zheng Tang <oscartang1120@gmail.com>
2020-11-06 15:12:03 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -37,14 +37,14 @@ trainer = GCNTrainer(opt)
 trainer.load(model_file)
 
 # load vocab
-vocab_file = args.model_dir + '/vocab.pkl'
+vocab_file = 'dataset/vocab/vocab.pkl'
 vocab = Vocab(vocab_file, load=True)
 assert opt['vocab_size'] == vocab.size, "Vocab size must match that in the saved model."
 
 # load data
 data_file = opt['data_dir'] + '/{}.json'.format(args.dataset)
 print("Loading data from {} with batch size {}...".format(data_file, opt['batch_size']))
-batch = DataLoader(data_file, opt['batch_size'], opt, vocab, evaluation=True)
+batch = DataLoader(data_file, opt['batch_size'], opt, vocab, opt['data_dir'] + '/mappings_{}.txt'.format(args.dataset), evaluation=True)
 
 helper.print_config(opt)
 label2id = constant.LABEL_TO_ID
