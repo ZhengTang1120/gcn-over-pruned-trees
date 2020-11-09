6ba2ba11ad09d216e011939c5582722f447fca09
Zheng Tang <oscartang1120@gmail.com>
2020-11-09 05:28:08 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -120,12 +120,15 @@ class GCNTrainer(Trainer):
             if self.opt['cuda']:
                 output = output.cuda()
         loss += loss_d/max_len if max_len != 0 else loss_d
-        loss_val = loss.item()
-        # backward
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.classifier.parameters(), self.opt['max_grad_norm'])
-        torch.nn.utils.clip_grad_norm_(self.decoder.parameters(), self.opt['max_grad_norm'])
-        self.optimizer.step()
+        if loss != 0:
+            loss_val = loss.item()
+            # backward
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.classifier.parameters(), self.opt['max_grad_norm'])
+            torch.nn.utils.clip_grad_norm_(self.decoder.parameters(), self.opt['max_grad_norm'])
+            self.optimizer.step()
+        else:
+            loss_val = 0
         return loss_val
 
     def predict(self, batch, unsort=True):
