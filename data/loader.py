62c0b904340572a9c9e32e22758fab8370fd3195
Zheng Tang <oscartang1120@gmail.com>
2020-11-10 02:33:21 -0700

    ssl

--- data/loader.py
+++ data/loader.py
@@ -75,7 +75,8 @@ class DataLoader(object):
                 rule = helper.word_tokenize(rules[eval(mappings[c])[0][1]])
                 rule = map_to_ids(rule, vocab.rule2id) 
                 rule = [constant.SOS_ID] + rule + [constant.EOS_ID]
-                relation = self.label2id[eval(mappings[c])[0][0]]
+                if not self.eval:
+                    relation = self.label2id[eval(mappings[c])[0][0]]
             else:
                 rule = []
 
