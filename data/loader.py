91bd4d14fe64d12707c16e09f418b5d7bad70cc3
Zheng Tang <oscartang1120@gmail.com>
2020-11-11 00:38:43 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -34,7 +34,7 @@ class DataLoader(object):
 
 
         self.id2label = dict([(v,k) for k,v in self.label2id.items()])
-        self.labels = [self.id2label[d[-2]] for d in data]
+        self.labels = [self.id2label[d[-3]] for d in data]
         self.num_examples = len(data)
         
         # chunk into batches
