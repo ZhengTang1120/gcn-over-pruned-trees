c78b4ef327b4df34520d8c182cf34e9503439d5b
Zheng Tang <oscartang1120@gmail.com>
2020-11-13 22:10:34 -0700

    decoder

--- train.py
+++ train.py
@@ -173,8 +173,9 @@ for epoch in range(1, opt['num_epoch']+1):
                         candidate.append(vocab.id2rule[int(r)])
                 # print (reference)
                 # print (candidate)
-                references.append(reference)
-                candidates.append(candidate)
+                if len(reference[0])!=0:
+                    references.append(reference)
+                    candidates.append(candidate)
 
     predictions = [id2label[p] for p in predictions]
     train_loss = train_loss / train_batch.num_examples * opt['batch_size'] # avg loss per batch
@@ -184,7 +185,7 @@ for epoch in range(1, opt['num_epoch']+1):
     bleu = corpus_bleu(references, candidates) if len(candidates)!=0 else 0
     print("epoch {}: train_loss = {:.6f}, dev_loss = {:.6f}, dev_f1 = {:.4f}, bleu = {:.4f}".format(epoch,\
         train_loss, dev_loss, dev_f1, bleu))
-    dev_score = dev_f1
+    dev_score = bleu
     file_logger.log("{}\t{:.6f}\t{:.6f}\t{:.4f}\t{:.4f}".format(epoch, train_loss, dev_loss, dev_score, max([dev_score] + dev_score_history)))
 
     # save
