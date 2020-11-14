7e37b607bca1a1a8e84c5435beea31b964636fc5
Zheng Tang <oscartang1120@gmail.com>
2020-11-14 04:59:45 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -118,7 +118,7 @@ class GCNTrainer(Trainer):
             output = rules.data[t]
             if self.opt['cuda']:
                 output = output.cuda()
-        loss += loss_d
+        loss += loss_d/10
         loss_val = loss.item()
         # backward
         loss.backward()
