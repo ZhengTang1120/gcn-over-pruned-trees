1eebb4b121d472f48ae3fc9d37682b1516bfea46
Zheng Tang <oscartang1120@gmail.com>
2020-11-14 04:20:59 -0700

    d

--- train.py
+++ train.py
@@ -185,7 +185,7 @@ for epoch in range(1, opt['num_epoch']+1):
     bleu = corpus_bleu(references, candidates) if len(candidates)!=0 else 0
     print("epoch {}: train_loss = {:.6f}, dev_loss = {:.6f}, dev_f1 = {:.4f}, bleu = {:.4f}".format(epoch,\
         train_loss, dev_loss, dev_f1, bleu))
-    dev_score = bleu
+    dev_score = dev_f1
     file_logger.log("{}\t{:.6f}\t{:.6f}\t{:.4f}\t{:.4f}".format(epoch, train_loss, dev_loss, dev_score, max([dev_score] + dev_score_history)))
 
     # save
