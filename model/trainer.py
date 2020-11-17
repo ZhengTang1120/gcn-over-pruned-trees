12bdb4d84a400dca7ad0a6da73b09ff73010c0b8
Zheng Tang <oscartang1120@gmail.com>
2020-11-17 00:37:14 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -120,7 +120,7 @@ class GCNTrainer(Trainer):
                 output = rules.data[t]
                 if self.opt['cuda']:
                     output = output.cuda()
-            loss += loss_d#/max_len if (self.opt['classifier'] and max_len!=0) else loss_d
+            loss += loss_d/max_len if (self.opt['classifier'] and max_len!=0) else loss_d
         if loss != 0:
             loss_val = loss.item()
             # backward
