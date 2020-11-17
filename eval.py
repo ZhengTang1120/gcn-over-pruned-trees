a9ba1c28b3369ece56d373e0646259ff3e581507
Zheng Tang <oscartang1120@gmail.com>
2020-11-17 00:36:34 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -82,9 +82,11 @@ for c, b in enumerate(batch_iter):
                 candidates.append(candidate)
 
 predictions = [id2label[p] for p in predictions]
-p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
-bleu = corpus_bleu(references, candidates)
-print("{} set evaluate result: {:.2f}\t{:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,p,r,f1,bleu))
+for pred in predictions:
+    print (pred)
+# p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
+# bleu = corpus_bleu(references, candidates)
+# print("{} set evaluate result: {:.2f}\t{:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,p,r,f1,bleu))
 
-print("Evaluation ended.")
+# print("Evaluation ended.")
 
