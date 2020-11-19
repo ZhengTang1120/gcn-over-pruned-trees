d42766c3686fa001b5f4ea05fea6f3e0bc805084
Zheng Tang <oscartang1120@gmail.com>
2020-11-19 03:50:52 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -78,11 +78,10 @@ for c, b in enumerate(batch_iter):
                 else:
                     candidate.append(vocab.id2rule[int(r)])
             if len(batch.refs[x][0])!=0:
-                if reference[0] != candidate:
-                    print (id2label[preds[i]], batch.gold()[x])
-                    print (batch.refs[x])
-                    print (candidate)
-                    print ()
+                print (id2label[preds[i]], batch.gold()[x])
+                print (batch.refs[x])
+                print (candidate)
+                print ()
 
                 references.append(batch.refs[x])
                 candidates.append(candidate)
