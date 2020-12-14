f2c17b398a67519372fc49925012bf9caa6bd07b
Zheng Tang <oscartang1120@gmail.com>
2020-12-13 23:01:51 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -79,7 +79,7 @@ exact_match = 0
 other = 0
 rule_set = set()
 rule_set2 = set()
-for b in batch:#enumerate(batch_iter):
+for c, b in enumerate(batch_iter):
     preds, probs, decoded, loss = trainer.predict(b)
     predictions += preds
     all_probs += probs
