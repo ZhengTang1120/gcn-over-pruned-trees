0781ff82a32e005273062c9f964b86fd372b3dda
Zheng Tang <oscartang1120@gmail.com>
2021-02-16 05:51:15 -0700

    Update bert.py

--- model/bert.py
+++ model/bert.py
@@ -17,7 +17,7 @@ class BERTclassifier(nn.Module):
     def forward(self, inputs):
         words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs
         outputs = self.model(**words)
-        outputs = outputs.last_hidden_state
+        outputs = outputs.pooler_output
         print (outputs.size())
         logits = self.classifier(outputs)
         return logits, None, None, None
