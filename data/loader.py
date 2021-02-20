d424f2be556019819c5c327a1a700a1b3ca167b6
Zheng Tang <oscartang1120@gmail.com>
2021-02-20 14:36:09 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -126,6 +126,8 @@ class DataLoader(object):
         print (batch[-1])
         words = self.tokenizer(batch[-1], padding=True, return_tensors="pt")
         print (words)
+        for ids in words.input_ids:
+            print [self.tokenizer.ids_to_tokens[i] for i in ids]
         masks = torch.eq(tokens, 0)
         pos = get_long_tensor(batch[1], batch_size)
         ner = get_long_tensor(batch[2], batch_size)
