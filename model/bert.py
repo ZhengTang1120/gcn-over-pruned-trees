be30e043bf6434b9676594eadb5080ef676eeb39
Zheng Tang <oscartang1120@gmail.com>
2021-02-21 06:04:28 -0700

    Update bert.py

--- model/bert.py
+++ model/bert.py
@@ -21,6 +21,7 @@ class BERTclassifier(nn.Module):
         words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs
         subj_mask, obj_mask = subj_pos.eq(0).eq(0).unsqueeze(2), obj_pos.eq(0).eq(0).unsqueeze(2)
         outputs = self.model(words)
+        print (outputs)
         h = outputs.last_hidden_state
         pool_type = self.opt['pooling']
         subj_out = F.tanh(self.linear(pool(h, subj_mask, type=pool_type)))
