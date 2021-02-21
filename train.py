b5409c46e0feb01746f33f979464a1e004a68bd2
Zheng Tang <oscartang1120@gmail.com>
2021-02-21 03:17:20 -0700

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from utils.vocab import Vocab
 
 from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
 
-from transformers import BertTokenizer
+from transformers import AutoTokenizer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', type=str, default='dataset/tacred')
@@ -104,7 +104,7 @@ assert emb_matrix.shape[0] == vocab.size
 assert emb_matrix.shape[1] == opt['emb_dim']
 
 # load data
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 special_tokens_dict = {'additional_special_tokens': constant.ENTITY_TOKENS}
 num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
 print("Loading data from {} with batch size {}...".format(opt['data_dir'], opt['batch_size']))
