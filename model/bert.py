2b67b55793ff8741011c76ea7f1601e4101260a8
Zheng Tang <oscartang1120@gmail.com>
2021-02-23 20:10:42 -0700

    subj obj

--- model/bert.py
+++ model/bert.py
@@ -10,26 +10,26 @@ from utils import constant, torch_utils
 class BERTclassifier(nn.Module):
     def __init__(self, opt, emb_matrix=None):
         super().__init__()
-        in_dim = 1024
-        self.model = BertModel.from_pretrained("bert-large-cased")
-        # self.linear = nn.Linear(in_dim, in_dim)
-        # self.classifier1 = nn.Linear(in_dim*3, 400)
-        # self.classifier2 = nn.Linear(400, opt['num_class'])
-        self.classifier = nn.Linear(in_dim, opt['num_class'])
+        in_dim = 768
+        self.model = BertModel.from_pretrained("bert-base-cased")
+        self.linear = nn.Linear(in_dim, in_dim)
+        self.classifier1 = nn.Linear(in_dim*3, 400)
+        self.classifier2 = nn.Linear(400, opt['num_class'])
+        # self.classifier = nn.Linear(in_dim, opt['num_class'])
         self.opt = opt
 
     def forward(self, inputs):
         words, masks, pos, ner, deprel, head, subj_pos, obj_pos, subj_type, obj_type = inputs
-        # subj_mask, obj_mask = subj_pos.eq(1000).eq(0).unsqueeze(2), obj_pos.eq(1000).eq(0).unsqueeze(2)
+        subj_mask, obj_mask = subj_pos.eq(1000).eq(0).unsqueeze(2), obj_pos.eq(1000).eq(0).unsqueeze(2)
         outputs = self.model(words)
-        # h = outputs.last_hidden_state
-        # pool_type = self.opt['pooling']
-        # subj_out = F.tanh(self.linear(pool(h, subj_mask, type=pool_type)))
-        # obj_out = F.tanh(self.linear(pool(h, obj_mask, type=pool_type)))
+        h = outputs.last_hidden_state
+        pool_type = self.opt['pooling']
+        subj_out = F.tanh(self.linear(pool(h, subj_mask, type=pool_type)))
+        obj_out = F.tanh(self.linear(pool(h, obj_mask, type=pool_type)))
         cls_out = outputs.pooler_output
-        # outputs = torch.cat([cls_out, subj_out, obj_out], dim=1)
-        # logits = self.classifier2(F.tanh(self.classifier1(outputs)))
-        logits = self.classifier(cls_out)
+        outputs = torch.cat([cls_out, subj_out, obj_out], dim=1)
+        logits = self.classifier2(F.tanh(self.classifier1(outputs)))
+        # logits = self.classifier(cls_out)
         return logits, None, None, None
 
 def pool(h, mask, type='max'):
