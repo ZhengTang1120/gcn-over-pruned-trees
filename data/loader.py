c9a03afc6a7de3b2b475f28a30e7319e8254d8b0
Zheng Tang <oscartang1120@gmail.com>
2021-03-03 03:43:02 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -74,11 +74,11 @@ class DataLoader(object):
                 for i in range(len(masked)):
                     if masked[i] < min(os, ss):
                         masked[i] += 1
-                    elif masked[i] < min(se,oe)+1:
+                    elif masked[i] <= min(se,oe):
                         masked[i] += 2
-                    elif masked[i] < max(os, ss)+2:
+                    elif masked[i] < max(os, ss):
                         masked[i] += 3
-                    elif masked[i] < max(se, oe)+3:
+                    elif masked[i] <= max(se, oe):
                         masked[i] += 4
                     else:
                         masked[i] += 5
