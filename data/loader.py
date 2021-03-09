b67c4110db7b7f03e828abcba0b4b988203b2bfe
Zheng Tang <oscartang1120@gmail.com>
2021-03-09 01:54:40 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -53,6 +53,10 @@ class DataLoader(object):
             patterns = f.readlines()
         # with open('dataset/tacred/rules.json') as f:
         #     rules = json.load(f)
+        ones = 0
+        twos = 0
+        threes = 0
+        zeros = 0
         for c, d in enumerate(data):
             tokens = list(d['token'])
             if opt['lower']:
@@ -101,20 +105,27 @@ class DataLoader(object):
                 tokens.insert(ss, '#')
                 tokens.insert(se+2, '#')
             tokens = ['[CLS]'] + tokens
-            tagging = [0 if i not in masked else 1 if tokens[i] in pattern else 2 for i in range(len(tokens))]
+            tagging = [0 if i not in masked else 1 if tokens[i] in pattern else 3 if 'SUBJ-' in tokens[i] or 'OBJ-' in tokens[i] else 2 for i in range(len(tokens))]
             if has_tag:
                 one = 0
                 two = 0
+                three = 0
                 zero = 0
                 for t in tagging:
                     if t == 0:
                         zero += 1
+                        zeros += 1
                     if t == 1:
                         one += 1
+                        ones += 1
                     if t == 2:
                         two += 1
+                        twos += 1
+                    if t == 3:
+                        three += 1
+                        threes += 1
                 print ([(tokens[i], tagging[i]) for i in range(len(tokens))])
-                print (one, two, zero)
+                print (one, two, three, zero)
             tokens = self.tokenizer.convert_tokens_to_ids(tokens)
             # tokens = map_to_ids(tokens, vocab.word2id)
             pos = map_to_ids(d['stanford_pos'], constant.POS_TO_ID)
@@ -129,6 +140,7 @@ class DataLoader(object):
             obj_type = [constant.OBJ_NER_TO_ID[d['obj_type']]]
             relation = self.label2id[d['relation']]
             processed += [(tokens, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, relation, tagging, has_tag)]
+        print (ones, twos, threes, zeros)
         return processed
 
     def gold(self):
