503299f24b50b5e3a22c6ded64e3c054a38e8a03
Zheng Tang <oscartang1120@gmail.com>
2021-03-10 07:08:13 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -144,7 +144,7 @@ class BERTtrainer(Trainer):
     def predict(self, batch, unsort=True):
         inputs, labels, rules, tokens, head, subj_pos, obj_pos, lens, tagged = unpack_batch(batch, self.opt['cuda'])
         rules = rules.data.cpu().numpy().tolist()
-        tokens = tokens.data.cpu.numpy().tolist()
+        tokens = tokens.data.cpu().numpy().tolist()
         orig_idx = batch[11]
         # forward
         self.classifier.eval()
