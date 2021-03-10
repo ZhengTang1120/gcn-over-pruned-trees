b7767a766f399847b4548ea8ad4248f9d53baf10
Zheng Tang <oscartang1120@gmail.com>
2021-03-10 05:47:37 -0700

    fixed eval bug

--- train.py
+++ train.py
@@ -173,7 +173,7 @@ for epoch in range(1, opt['num_epoch']+1):
     count = 0
     inputs = []
     for _, batch in enumerate(dev_batch):
-        preds, ts, tagged = trainer.predict(batch)
+        preds, ts, tagged, ids = trainer.predict(batch)
         # preds, _, decoded, loss = trainer.predict(batch)
         predictions += preds
         tags += ts
@@ -181,7 +181,6 @@ for epoch in range(1, opt['num_epoch']+1):
         dev_loss += loss
         batch_size = len(preds)
         for i in range(batch_size):
-            ids = batch[0][i]
             inputs += [[tokenizer.convert_ids_to_tokens(i) for i in ids.cpu().numpy().tolist()]]
         #     print (id2label[preds[i]], dev_batch.gold()[x])
         #     x += 1
