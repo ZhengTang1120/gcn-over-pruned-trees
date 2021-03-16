e4ed0c281b7783301f20516a6c4d18178d5c0655
Zheng Tang <oscartang1120@gmail.com>
2021-03-16 00:52:57 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -107,7 +107,7 @@ class BERTtrainer(Trainer):
                     loss = self.criterion2(tagging_output[i].squeeze(1), rules[i].to(torch.float32))
                 else:
                     loss += self.criterion2(tagging_output[i], rules[i])
-                logits = self.classifier(h[i], rules[i].unsqueeze(0))
+                logits = self.classifier(h[i], rules[i].eq(0).unsqueeze(0))
                 loss += self.criterion(logits, labels[i])
             else:
                 tag_cands = self.tagger.generate_cand_tags(tagging_output[i])
