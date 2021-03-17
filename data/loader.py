d286e9231765ffafb779c1ded0c84d6a034f8f49
Zheng Tang <oscartang1120@gmail.com>
2021-03-16 22:49:54 -0700

    ...

--- data/loader.py
+++ data/loader.py
@@ -102,7 +102,7 @@ class DataLoader(object):
             if has_tag:
                 tagging = [0 if i not in masked else 1 if tokens[i] in pattern else 0 for i in range(len(tokens))]
             else:
-                tagging = [1 for i in range(len(tokens))]
+                tagging = [1 if i !=0 else 0 for i in range(len(tokens))]
             tokens = self.tokenizer.convert_tokens_to_ids(tokens)
             pos = map_to_ids(d['stanford_pos'], constant.POS_TO_ID)
             ner = map_to_ids(d['stanford_ner'], constant.NER_TO_ID)
