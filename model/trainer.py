44ed54b7e0b106d33895abdb28c38e310a9a97da
Zheng Tang <oscartang1120@gmail.com>
2021-03-16 17:29:48 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -98,7 +98,7 @@ class BERTtrainer(Trainer):
             # decoder
             for i, f in enumerate(tagged):
                 if f:
-                    loss += self.criterion2(tagging_output[i], rules[i].unsqueeze(2).to(torch.float32))
+                    loss += self.criterion2(tagging_output[i], rules[i].unsqueeze(1).to(torch.float32))
         if loss != 0:
             loss_val = loss.item()
             # backward
