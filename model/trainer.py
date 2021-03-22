669ab68adfc7db09be93ecbfb2fb0b4c378c52f6
Zheng Tang <oscartang1120@gmail.com>
2021-03-22 16:55:25 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -100,7 +100,7 @@ class BERTtrainer(Trainer):
         h, b_out = self.encoder(inputs)
         tagging_output = self.tagger(h)
         loss = self.criterion2(b_out, (~(labels.eq(0))).to(torch.float32).unsqueeze(1))
-        if epoch < 0:
+        if epoch <= 1:
             logits = self.classifier(h, inputs[1], inputs[6], inputs[7])
             loss += self.criterion(logits, labels)
             for i, f in enumerate(tagged):
