a17d9deefe1881c2e0ea47d2c8788c59edd2cd97
Zheng Tang <oscartang1120@gmail.com>
2021-03-22 17:48:13 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -102,7 +102,7 @@ class BERTtrainer(Trainer):
         loss = self.criterion2(b_out, (~(labels.eq(0))).to(torch.float32).unsqueeze(1))
         if epoch <= 1:
             logits = self.classifier(h, inputs[1], inputs[6], inputs[7])
-            print (logits[0].size(), labels[0].size())
+            print (logits[0].size(), labels.unsqueeze(1)[0].size())
             print (logits.size(), labels.size())
             loss += self.criterion(logits, labels)
             for i, f in enumerate(tagged):
