378a47417355c769cb699614d68fce78a9e93700
Zheng Tang <oscartang1120@gmail.com>
2021-03-29 16:45:34 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -170,7 +170,7 @@ class BERTtrainer(Trainer):
         #         #     # print (r)
         #         # elif sum(t)!=0:
         #         #     # pass
-                    print (sum(a[i][-1][0]))
+                    print (a[i][-1][0])
                     exit()
                     print (id2label[p], id2label[labels.data.cpu().numpy().tolist()[i]])
                     print ([(a[i][-1][j], tokenizer.convert_ids_to_tokens(tokens[i][j])) for j in range(len(tokens[i])) if tokens[i][j] != 0])
