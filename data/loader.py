b884286b92290f7969b8509d935027dfe9cb752d
Zheng Tang <oscartang1120@gmail.com>
2021-03-31 03:33:59 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -129,7 +129,7 @@ class DataLoader(object):
             subj_type = [constant.SUBJ_NER_TO_ID[d['subj_type']]]
             obj_type = [constant.OBJ_NER_TO_ID[d['obj_type']]]
             processed += [(tokens, pos, ner, deprel, head, subj_positions, obj_positions, subj_type, obj_type, relation, tagging, has_tag)]
-            exit()
+        exit()
         return processed
 
     def gold(self):
