866f9b76bff630a4b6c10208de16cfcd85aa750f
Zheng Tang <oscartang1120@gmail.com>
2021-03-31 01:38:09 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -167,14 +167,14 @@ class BERTtrainer(Trainer):
                 tags += [t]
                 if sum(rules[i])!=0 and tagged:
                     pass
-                    r = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(rules[i])
-                    pr = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(t) if sum(t)!=0 else 0
-                    print ('%d, %d, %d, %.6f, %.6f'%(sum(t), len(t), l, r, pr))
-                # elif sum(t)!=0 and sum(t) in range(l-2, l+2):
-                # #     # pass
-                #     print ('%d, %d, %d'%(sum(t), len(t), l))
-                #     print (id2label[p], id2label[labels.data.cpu().numpy().tolist()[i]])
-                #     print ([(t[j], tokenizer.convert_ids_to_tokens(tokens[i][j])) if t[j]!=0 else tokenizer.convert_ids_to_tokens(tokens[i][j]) for j in range(len(tokens[i])) if tokens[i][j] != 0])
+                    # r = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(rules[i])
+                    # pr = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(t) if sum(t)!=0 else 0
+                    # print ('%d, %d, %d, %.6f, %.6f'%(sum(t), len(t), l, r, pr))
+                elif sum(t)!=0 and sum(t) in range(l-2, l+2):
+                #     # pass
+                    print ('%d, %d, %d'%(sum(t), len(t), l))
+                    # print (id2label[p], id2label[labels.data.cpu().numpy().tolist()[i]])
+                    # print ([(t[j], tokenizer.convert_ids_to_tokens(tokens[i][j])) if t[j]!=0 else tokenizer.convert_ids_to_tokens(tokens[i][j]) for j in range(len(tokens[i])) if tokens[i][j] != 0])
                 #     # print ()
             else:
                 tags += [[]]
