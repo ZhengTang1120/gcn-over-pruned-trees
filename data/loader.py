ba534a20240430c0811580761ac5dda3e6151a21
Zheng Tang <oscartang1120@gmail.com>
2021-03-31 12:33:31 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -109,13 +109,12 @@ class DataLoader(object):
                 ner.insert(se+2, '#')
             tokens = ['[CLS]'] + tokens
             relation = self.label2id[d['relation']]
-            tagging = [1 if i in masked else 0 for i in range(len(tokens))]
-            # if has_tag and relation!=0:
-            #     tagging = [0 if i not in masked else 1 if (tokens[i] in pattern or ner[i] in pattern) and tokens[i] not in string.punctuation else 0 for i in range(len(tokens))]
-            # # elif relation!=0:
-            # #     tagging = [1 if i !=0 else 0 for i in range(len(tokens))]
-            # else:
-            #     tagging = [0 for i in range(len(tokens))]
+            if has_tag and relation!=0:
+                tagging = [0 if i not in masked else 1 if (tokens[i] in pattern or ner[i] in pattern) and tokens[i] not in string.punctuation else 0 for i in range(len(tokens))]
+            # elif relation!=0:
+            #     tagging = [1 if i !=0 else 0 for i in range(len(tokens))]
+            else:
+                tagging = [0 for i in range(len(tokens))]
             tokens = self.tokenizer.convert_tokens_to_ids(tokens)
             pos = map_to_ids(d['stanford_pos'], constant.POS_TO_ID)
             ner = map_to_ids(d['stanford_ner'], constant.NER_TO_ID)
