fe7e6d769bdedbfeb4416ddaa22691b0ae06216e
Zheng Tang <oscartang1120@gmail.com>
2021-04-06 00:36:51 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -15,6 +15,8 @@ from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
 
 from transformers import BertTokenizer
 
+import json
+
 parser = argparse.ArgumentParser()
 parser.add_argument('model_dir', type=str, help='Directory of the model.')
 parser.add_argument('--model', type=str, default='best_model.pt', help='Name of the model file.')
@@ -75,19 +77,23 @@ for c, b in enumerate(batch):
     batch_size = len(preds)
     for i in range(batch_size):
         inputs += [[tokenizer.convert_ids_to_tokens(j) for j in ids[i]]]
+output = list()
 for i, p in enumerate(predictions):
         predictions[i] = id2label[p]
+        output.append({'gold_label':batch.gold()[i], 'predicted_label':id2label[p], 'raw_words':batch.words[i], 'predicted_tags':[], 'gold_tags':[]})
         if p!=0:
             if sum(goldt[i])!=0:
-                pass
+                output[-1]['gold_tags'] = [goldt[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
                 # print (id2label[p], batch.gold()[i])
                 # print ([(goldt[i][j], tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
                 # print ()
-            elif sum(tags[i])!=0:
-                pass
-                print (id2label[p], batch.gold()[i])
-                print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
-                print ()
+            if sum(tags[i])!=0:
+                output[-1]['predicted_tags'] = [tags[i][j] for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]']
+                # print (id2label[p], batch.gold()[i])
+                # print ([(tags[i][j], batch.words[i][j])for j in range(len(inputs[i])) if inputs[i][j] != '[PAD]'])
+                # print ()
+with open("output_{}".format(args.model), 'w') as f:
+    f.write(json.dumps(output))
 p, r, f1 = scorer.score(batch.gold(), predictions, verbose=True)
 print("{} set evaluate result: {:.2f}\t{:.2f}\t{:.2f}".format(args.dataset,p,r,f1))
 
