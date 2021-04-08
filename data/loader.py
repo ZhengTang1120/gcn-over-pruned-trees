8f80cd8f6aae0dce795267b629d07ce7ac401e39
Zheng Tang <oscartang1120@gmail.com>
2021-04-08 04:27:44 -0700

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -116,10 +116,10 @@ class DataLoader(object):
             # elif relation!=0:
             #     tagging = [1 if i !=0 else 0 for i in range(len(tokens))]
             else:
-                tagging = [1 if i in masked else 0 for i in range(len(tokens))]
-            if 'Presnyakova' in tokens:
-                print (pattern)
-                print ([(tokens[i], tagging[i], ner[i]) for i in range(len(tokens))])
+                tagging = [0 for i in range(len(tokens))]
+            # if 'Presnyakova' in tokens:
+            print (pattern)
+            print ([(tokens[i], tagging[i], ner[i]) for i in range(len(tokens))])
             tokens = self.tokenizer.convert_tokens_to_ids(tokens)
             pos = map_to_ids(d['stanford_pos'], constant.POS_TO_ID)
             ner = map_to_ids(d['stanford_ner'], constant.NER_TO_ID)
