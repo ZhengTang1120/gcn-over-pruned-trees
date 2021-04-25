f4dbc161dc70200de5dbf60416736f524a0f7f88
Zheng Tang <oscartang1120@gmail.com>
2021-04-25 16:15:57 -0700

    Update trainer.py

--- model/trainer.py
+++ model/trainer.py
@@ -171,12 +171,12 @@ class BERTtrainer(Trainer):
                 t = t.data.cpu().numpy().tolist()
                 l = lens.data.cpu().numpy().tolist()[i]
                 tags += [t]
-                # if sum(rules[i])!=0 and tagged:
-                #     r = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(rules[i])
-                #     pr = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(t) if sum(t)!=0 else 0
-                #     r2 = sum([1 if chunk[j]==rules[i][j] else 0 for j in range(len(chunk)) if rules[i][j]!=0])/sum(rules[i])
-                #     pr2 = sum([1 if chunk[j]==rules[i][j] else 0 for j in range(len(chunk)) if rules[i][j]!=0])/sum(chunk) if sum(chunk)!=0 else 0
-                #     print ('%.6f, %.6f, %.6f, %.6f'%(r, pr, r2, pr2))
+                if sum(rules[i])!=0 and tagged:
+                    r = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(rules[i])
+                    pr = sum([1 if t[j]==rules[i][j] else 0 for j in range(len(t)) if rules[i][j]!=0])/sum(t) if sum(t)!=0 else 0
+                    r2 = sum([1 if chunk[j]==rules[i][j] else 0 for j in range(len(chunk)) if rules[i][j]!=0])/sum(rules[i])
+                    pr2 = sum([1 if chunk[j]==rules[i][j] else 0 for j in range(len(chunk)) if rules[i][j]!=0])/sum(chunk) if sum(chunk)!=0 else 0
+                    print ('%.6f, %.6f, %.6f, %.6f'%(r, pr, r2, pr2))
             else:
                 tags += [[]]
         if unsort:
