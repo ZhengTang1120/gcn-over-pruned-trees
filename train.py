c81400385e65bc8e470c20094b951ac777f6f7c4
Zheng Tang <oscartang1120@gmail.com>
2021-04-29 16:07:41 -0700

    Update train.py

--- train.py
+++ train.py
@@ -105,8 +105,8 @@ assert emb_matrix.shape[1] == opt['emb_dim']
 
 # load data
 tokenizer = BertTokenizer.from_pretrained('mrm8488/spanbert-large-finetuned-tacred')
-special_tokens_dict = {'additional_special_tokens': constant.ENTITY_TOKENS}
-num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
+# special_tokens_dict = {'additional_special_tokens': constant.ENTITY_TOKENS}
+# num_added_toks = tokenizer.add_special_tokens(special_tokens_dict)
 # print (tokenizer.get_vocab())
 print("Loading data from {} with batch size {}...".format(opt['data_dir'], opt['batch_size']))
 train_batch = DataLoader(opt['data_dir'] + '/train.json', opt['batch_size'], opt, vocab, opt['data_dir'] + '/interval_train.txt', opt['data_dir'] + '/pattern_train.txt', tokenizer, evaluation=False)
